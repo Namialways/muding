f7d8925063faa28b56b4d75f00794bef23e847d2
zhangzihao <15138288340@163.com>
2026-05-05 18:53:04 +0800

    build: 升级版本号至 1.1.2 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.muding.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
